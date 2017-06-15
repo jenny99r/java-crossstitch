ea063c111c599b81feb60f4bdfd640bdcdfed7ea
Jenny Ramsay <jennifer.ramsay@mergermarket.com>
2017-06-15 17:01:46 +0100

    Detecting lines - not working

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,7 +1,11 @@
+import com.sun.javafx.geom.Vec2f;
 import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.Vector;
+
 import static org.opencv.core.Core.bitwise_not;
+import static org.opencv.core.Core.line;
 import static org.opencv.core.CvType.CV_8UC1;
 import static org.opencv.highgui.Highgui.imread;
 import static org.opencv.highgui.Highgui.imwrite;
@@ -10,11 +14,13 @@ import static org.opencv.imgproc.Imgproc.*;
 public class Sudoku {
 
     static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+    
+    private static Mat KERNEL = getStructuringElement(MORPH_RECT, new Size(3, 3));
 
     public static void main(String[] args) {
         System.out.println("Welcome to OpenCV " + Core.VERSION);
 
-        Mat image = imread("cross.jpg", 0);
+        Mat image = imread("sudoku.jpg", 0);
 
         Mat outerBox = initaliseImage(image);
 
@@ -22,6 +28,12 @@ public class Sudoku {
 
         detectOutline(outerBox);
         imwrite("output/outline.jpg", outerBox);
+
+        erode(outerBox, outerBox, KERNEL);
+        imwrite("output/erode.jpg", outerBox);
+
+        detectLines(outerBox);
+        imwrite("output/lines.jpg", outerBox);
     }
 
     private static Mat initaliseImage(Mat image) {
@@ -33,9 +45,7 @@ public class Sudoku {
 
         bitwise_not(outerBox, outerBox);
 
-        Mat kernel = getStructuringElement(MORPH_RECT, new Size(3, 3));
-
-        dilate(outerBox, outerBox, kernel);
+        dilate(outerBox, outerBox, KERNEL);
         return outerBox;
     }
 
@@ -72,6 +82,30 @@ public class Sudoku {
                     floodFill(outerBox, newOuterBox, new Point(x,y), new Scalar(0));
                 }
             }
+        }
+    }
+
+    private static void detectLines(Mat outerBox) {
+        Mat lines = new Mat();
+        HoughLines(outerBox, lines, 1, Math.PI /180, 200);
+
+        for (int i = 0; i < lines.cols(); i++) {
+            drawLine(lines.get(0, i), outerBox, new Scalar(128));
+        }
+
+    }
+
+    private static void drawLine(double[] vals, Mat mat, Scalar rgb) {
+        System.out.println(vals.length + " " + vals[0] + " " + vals[1]);
+        double r = vals[0];
+        double theta = vals[1];
+        if(r !=0) {
+            double m = -1.0/Math.tan(r);
+            double c = r/Math.sin(theta);
+
+            line(mat, new Point(0, c), new Point(mat.size().width, m* mat.size().width + c), rgb);
+        } else {
+//            line(mat, new Point(0, 0), new Point(0, mat.size().height), rgb);
         }
     }
 }
